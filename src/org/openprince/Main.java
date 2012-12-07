08831f44156aaab41c9b3dce3fcf53b5ebb85749
Robert Biro <darthjdg@gmail.com>
2012-12-07 00:04:51 +0000

    Add simple SpriteSheet renderer to main
    I can finally display the images (and see that they're actually wrong).

--- src/org/openprince/Main.java
+++ src/org/openprince/Main.java
@@ -2,6 +2,10 @@ package org.openprince;
 
 import java.io.IOException;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 import org.openprince.dat.DefinitionManager;
 import org.openprince.dat.Reader;
 
@@ -18,8 +22,10 @@ public class Main {
 
 		System.out.println("Loading resources...");
 
+		Reader dat = null;
+
 		try {
-			Reader dat = new Reader("data/vdungeon.dat");
+			dat = new Reader("data/kid.dat");
 			dat.allocateSprites();
 
 			System.out.println("Sheets: " + dat.sheets.size());
@@ -28,5 +34,57 @@ public class Main {
 		}
 
 		System.out.println("done.");
+
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+		int texId = GL11.glGenTextures();
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texId);
+		GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
+				GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER,
+				GL11.GL_LINEAR);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, 512, 512, 0,
+				GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, dat.sheets.get(0).buffer);
+
+		// init OpenGL
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 600, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+		while (!Display.isCloseRequested()) {
+			// Clear the screen and depth buffer
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+			// set the color of the quad (R,G,B,A)
+			GL11.glColor3f(1.0f, 1.0f, 1.0f);
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glTexCoord2f(0, 0);
+			GL11.glVertex2f(0, 0);
+			GL11.glTexCoord2f(1.0f, 0);
+			GL11.glVertex2f(512, 0);
+			GL11.glTexCoord2f(1.0f, 1.0f);
+			GL11.glVertex2f(512, 512);
+			GL11.glTexCoord2f(0, 1.0f);
+			GL11.glVertex2f(0, 512);
+
+			GL11.glEnd();
+
+			Display.sync(30);
+			Display.update();
+		}
+
+		Display.destroy();
 	}
 }
